6f9ec5cef37eb82978c00f87521f2a907814a10c
Saptarshi Dey <dey7.kol@gmail.com>
2018-01-03 23:34:40 +0530

    modified register api

--- routes/register-user.js
+++ routes/register-user.js
@@ -9,11 +9,18 @@ module.exports = function(app){
         var lastName = req.body.lastName||null;
         var password = req.body.password||null;
 
+        const existingUser = await User.find().sort({_id:-1}).limit(1);
+        if(existingUser){
+            console.log(existingUser)
+            var userID = parseInt((existingUser[0].UserID).substr(4))+1;
+        } 
+        else
+            return
         const user = await new User({
             FirstName:firstName,
             LastName:lastName,
             Password:password,
-            UserID:'USER001'
+            UserID:'USER'+userID
         }).save();
 
         console.log(user)
