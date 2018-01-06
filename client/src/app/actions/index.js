29ad134b86175951393f226afd910bfec83d326f
Saptarshi Dey <dey7.kol@gmail.com>
2018-01-06 22:45:54 +0530

    actions added for calling apis

---
+++ client/src/app/actions/index.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+import {FETCH_USER} from './types'
+
+export const fetchUser = () => {
+
+    return async function(dispatch){
+        const res = await axios.get('/api/currentUser')
+        dispatch({type: FETCH_USER,payload:res.data})
+    }
+}
+
