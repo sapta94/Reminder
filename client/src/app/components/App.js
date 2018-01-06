83b6421f96a9e234428acc63ee80045326d7326d
Saptarshi Dey <dey7.kol@gmail.com>
2018-01-06 22:33:46 +0530

    Project structure modiifed

---
+++ client/src/app/components/App.js
@@ -0,0 +1,34 @@
+import React,{Component} from 'react'
+import {BrowserRouter,Route} from 'react-router-dom'
+import {connect} from 'react-redux'
+import * as actions from '../actions'
+
+import Navbar from './Navbar'
+import Dashboard from './Dashboard'
+import Landing from './Landing'
+import SurveyNew from './Surveys/SurveyNew'
+//const Dashboard = () => <h2>Dashboard</h2>
+//const SurveyNew = () => <h2>Survey New</h2>
+
+class App extends Component{
+    componentDidMount(){
+        this.props.fetchUser();
+    }
+
+    render() {
+        return(
+            <div className="container">
+            <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route exact path="/" component={Landing} />
+                        <Route exact path="/survey" component={Dashboard} />
+                        <Route exact path="/survey/new" component={SurveyNew} />
+                    </div>
+            </BrowserRouter>
+            </div>
+        )
+    }
+}
+
+export default connect (null,actions)(App);
--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,18 +1,11 @@
 import React from "react";
 import ReactDOM,{render} from "react-dom";
 import Navbar from "./components/Navbar"
+import App from "./components/App"
 import {Provider} from 'react-redux'
 import {createStore,applyMiddleware} from 'redux'
 
-class App extends React.Component{
-    render(){
-        return (
-            <div>
-                <Navbar />
-            </div>
-        )
-    }
-}
+
 const store = createStore(reducers,{})
 render(
 <Provider store={store}><App /></Provider>,window.document.getElementById('root'));
