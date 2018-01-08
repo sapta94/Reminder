257233260a0253e9831803a28db66c88bfeaa295
Saptarshi Dey <dey7.kol@gmail.com>
2018-01-08 23:01:17 +0530

    action added for login api

--- client/src/app/actions/index.js
+++ client/src/app/actions/index.js
@@ -9,3 +9,13 @@ export const fetchUser = () => {
     }
 }
 
+export const loginUser = (username,password) => {
+    return async function(dispatch){
+        const res = await axios.post('http://localhost:5000/api/login', {
+            username: username,
+            password: password
+          })
+        dispatch({type: FETCH_USER,payload:res.data})
+    }
+}
+
