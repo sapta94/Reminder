6738f2c14e85b1048afa42ee56c72d8797f991f9
Saptarshi Dey <dey7.kol@gmail.com>
2018-01-19 20:02:44 +0530

    viewing notification in cards

--- client/src/app/components/Dashboard.js
+++ client/src/app/components/Dashboard.js
@@ -2,6 +2,30 @@ import React,{Component} from 'react'
 import * as actions from '../actions'
 import {connect} from 'react-redux'
 
+class Section extends Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div class="row">
+                <div class="col s12 m6">
+                <div class="card blue-grey darken-1">
+                    <div class="card-content white-text">
+                    <span class="card-title">{this.props.title}</span>
+                    <p>{this.props.desc}</p>
+                    </div>
+                    <div class="card-action">
+                    <a href="#">{this.props.time}</a>
+                    <a href="#">Edit</a>
+                    </div>
+                </div>
+                </div>
+            </div>
+        )
+    }
+}
+
 class Dashboard extends Component{
     componentWillMount(){
         this.props.fetchNoti('USER2')
@@ -10,8 +34,12 @@ class Dashboard extends Component{
 
     render(){
         const notify=this.props.noti;
-        if(notify!=null)
+        if(notify!=null){
             console.log(notify.data)
+            return(
+                <Section title={notify.data[0].Title} desc={notify.data[0].Description} time={notify.data[0].createTime}/>
+            )
+        }
         return(
             <div style={{textAlign:'center'}}>
                 Dashboard
