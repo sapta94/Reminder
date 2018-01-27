14b3ac47c5a60d89169a0e8deeeadf50fdb22302
Saptarshi Dey <dey7.kol@gmail.com>
2018-01-27 20:55:27 +0530

    added create react app boilerplate

--- client/src/app/actions/types.js
+++
@@ -1,2 +0,0 @@
-export const VALID_USER ='valid_user'
-export const FETCH_USER = 'fetch_user'
--- client/src/app/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-import authReducer from './authReducer'
-import notiReducer from './notiReducer'
-//import {reducer as reduxForm} from 'redux-form'
-
-export default combineReducers({
-    auth:authReducer,
-    noti:notiReducer
-})
