f198cb5b121660272344f8c6031945c31477c7ec
Saptarshi Dey <dey7.kol@gmail.com>
2018-01-28 20:45:56 +0530

    editing update notification

--- client/src/components/Notification.js
+++ client/src/components/Notification.js
@@ -8,18 +8,20 @@ class Notification extends Component{
     constructor(props){
         super(props);
         this.state={
-            title:null,
-            description:null,
+            title:'',
+            description:'',
             notifyTime:'',
             submitted:false
         }
         this.handleChange=this.handleChange.bind(this)
         this.handleSubmit=this.handleSubmit.bind(this)
+        this.renderEditValues=this.renderEditValues.bind(this)
     }
 
     componentDidMount(){
         this.props.fetchUser();
         console.log(this.props.auth)
+        this.renderEditValues();
     }
 
     handleChange(e) {
@@ -41,32 +43,41 @@ class Notification extends Component{
         return decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
     }
 
+    renderEditValues(){
+        var prevTitle=this.getQueryString('title')||''
+        var prevDesc=this.getQueryString('description')||''
+        var prevNotifyTime=parseInt(this.getQueryString('notifyTime'))||''
+        console.log(prevNotifyTime)
+        var dateStr= moment.unix((prevNotifyTime)/1000).format("YYYY-MM-DD");
+        this.setState({
+            title:prevTitle,
+            description:prevDesc,
+            notifyTime:prevNotifyTime
+        })
+    }
+
     // callLogin(userID,password){
     //     console.log('login called')
     //     this.props.loginUser(userID,password)
     // }
 
     render(){
-        var prevTitle=this.getQueryString('title')||''
-        var prevDesc=this.getQueryString('description')||''
-        var prevNotifyTime=parseInt(this.getQueryString('notifyTime'))||''
-        console.log(prevNotifyTime)
-        var dateStr= moment.unix((prevNotifyTime)/1000).format("YYYY-MM-DD");
+        
         return(
             <div className="row">
                 <form className="col s12">
                     <div className="row">
                         <div className="input-field col s4">
-                            <input  onChange={this.handleChange} name="title" type="text"  className="validate" />
-                            <label for="first_name">Title</label>
+                            <input  onChange={this.handleChange} name="title" type="text" value={this.state.title} className="validate" />
+                            <label for="title">Title</label>
                         </div>
                         <div className="input-field col s8">
-                            <input onChange={this.handleChange} name="description"  type="text" className="validate" />
-                            <label for="last_name">Description</label>
+                            <input onChange={this.handleChange} name="description" value={this.state.description} type="text" className="validate" />
+                            <label for="description">Description</label>
                         </div>
                         <div className="input-field col s6">
-                            <input onChange={this.handleChange} name="notifyTime"  type="date" className="validate" />
-                            <label for="last_name">Date & Time</label>
+                            <input onChange={this.handleChange} name="notifyTime" value={this.state.notifyTime} type="date" className="validate" />
+                            <label for="notifyTime">Date & Time</label>
                         </div>
                     </div>
                     <a onClick={this.handleSubmit} className="waves-effect waves-light btn">Set Reminder</a>
