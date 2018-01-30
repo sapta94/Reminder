a3b638a37784d35ba100e9d8a03b86408e09b00d
Saptarshi Dey <dey7.kol@gmail.com>
2018-01-30 22:53:57 +0530

    changes in auth reducer to redirect after register

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -3,6 +3,11 @@ import {FETCH_USER} from '../actions/types'
 export default function (state=null,action) {
     //console.log(action)
     switch(action.type){
+        case '':
+        if(action.payload.message=='success'){
+            window.location.href="/login"
+        }
+        return action.payload||false
         case 'VALID_USER':
             console.log(action.payload)
             // if(action.payload.message=='success'){
