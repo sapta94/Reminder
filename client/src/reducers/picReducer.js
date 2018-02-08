cc23f8c2128b5a6d500c38ba4a2e63d8815ac5e5
Saptarshi Dey <dey7.kol@gmail.com>
2018-02-08 22:40:50 +0530

    reducer added to handle it

---
+++ client/src/reducers/picReducer.js
@@ -0,0 +1,9 @@
+export default function (state=null,action) {
+    //console.log(action)
+    switch(action.type){
+        case 'PRO_PIC':
+            return action.payload||false
+        default :
+            return state
+    }
+}
